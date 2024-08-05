b60d44880b787d96782579331a34ecd656c34849
TALHA IQBAL <134729186+hackerspidy1@users.noreply.github.com>
2024-08-05 12:23:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
-import connectDB from "./DB/mongodb.js";
-import cors from "cors";
-import express from "express";
-import productRoute from "./route/Product.js";
-import { configDotenv } from "dotenv";
+import express from 'express';
+import connectDB from './DB/mongodb.js';
+import cors from 'cors';
+import productRoute from './route/Product.js';
+import uploadRouter from './route/upload.js';
+import categoryRoute from './route/categorey.js';
+import orderRoute from './route/Order.js';
+import messageRoute from './route/Messages.js';
+import featureProduct from './route/Feature.js';
+import Auth from './route/AuthRoute.js';
+import { config } from 'dotenv';
 import fs from 'fs';
-import uploadRouter from "./route/upload.js";
-import categoryRoute from "./route/categorey.js";
-import orderRoute from "./route/Order.js";
-import messageRoute from "./route/Messages.js";
-import featureProduct from "./route/Feature.js";
-import Auth from "./route/AuthRoute.js";
-import { baseURL } from "./constants/constant.js";
 
+// Initialize environment variables
+config();
+
+// Create an instance of Express
 const app = express();
 
-
-//middlewares
+// Middleware setup
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-
 
 // Create 'uploads' directory if it doesn't exist
 if (!fs.existsSync('uploads')) {
   fs.mkdirSync('uploads');
 }
 
-// Use the upload route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Use the routes
 app.use('/api', uploadRouter);
 app.use('/api', productRoute);
 app.use('/api', categoryRoute);
 app.use('/api', orderRoute);
 app.use('/api', messageRoute);
-app.use('/api',featureProduct)
-app.use("/api", Auth);
+app.use('/api', featureProduct);
+app.use('/api', Auth);
 
-
-
-
-// Load environment variables
-configDotenv();
+// Connect to the database and handle errors
 connectDB()
   .then(() => {
-    const port = process.env.PORT ; // Use process.env.PORT if available, otherwise default to 8000
+    // Start the server after the database connection is successful
+    const port = process.env.PORT || 8000; // Default to port 8000 if PORT is not set
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+// Export the Express app
+export default app;
